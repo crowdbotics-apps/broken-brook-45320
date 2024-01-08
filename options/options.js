35da210ee9447889aeb86044e44b80c911ae279b
Crowdbotics <team@crowdbotics.com>
2024-01-08 13:25:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-brook-45320",
+  url: "https://broken-brook-45320.botics.co",
+  api: "https://broken-brook-45320.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
